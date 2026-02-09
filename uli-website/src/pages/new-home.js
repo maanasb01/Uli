ceda047784dc6d54b8682a6d578bb56d1ecca32b
maanasb01 <maanasb01@gmail.com>
2026-02-09 13:04:28 +0530

    feat: add main heading, and Labrada font to tailwind

--- uli-website/src/pages/new-home.js
+++ uli-website/src/pages/new-home.js
@@ -12,10 +12,26 @@ const NewHome = () => {
     <AppShellNew>
       <Box align="center" margin={{ horizontal: "large", vertical: "small" }}>
         <Announcements />
-        <Box className="flex flex-col items-center pb-12 gap-14">
-          <p className="text-[150px] leading-[90%] tracking-[-5%] mx-auto mt-14 mb-0">Reclaim your <br/> online space</p>
+        <Box className="flex flex-col items-center pb-12 gap-14 mt-8">
+          <div className="relative">
+            <img
+              src="/heading.svg"
+              alt="Reclaim your online space"
+              className="pointer-events-none block h-auto "
+            />
+            {/* <p
+              className="text-[200px] leading-[90%] tracking-[-5] font-labrada text-transparent absolute select-none"
+              aria-hidden="true"
+            >
+              Reclaim your <br /> online space
+            </p> */}
+          </div>
 
-          <Text className="w-[30%] text-center">Short Statement about what Uli isUse Uli to redact slurs and abusive content, archive problematic content, and collectively push back against online gender based violence.</Text>
+          <Text className="w-[30%] text-center">
+            Short Statement about what Uli isUse Uli to redact slurs and abusive
+            content, archive problematic content, and collectively push back
+            against online gender based violence.
+          </Text>
 
           <Box className="flex flex-row gap-8 font-bold">
             <Text className="cursor-pointer">Our Process</Text>
